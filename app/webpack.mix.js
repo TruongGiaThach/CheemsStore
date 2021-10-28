bc5bc87a9d7b6bd0b362f8cb0d10161a9b434fc6
TruongGiaThach <71328121+TruongGiaThach@users.noreply.github.com>
2021-10-28 21:48:29 +0700

    test_frondend

--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -14,4 +14,6 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ])
+    .js('resources/assets/js/bootstrap.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css');
