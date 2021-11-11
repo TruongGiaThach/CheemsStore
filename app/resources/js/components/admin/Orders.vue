99dcde3eaf27b4eaf56aca66ffb5fbb9a89726b6
Dung-kun <71446604+Dung-kun@users.noreply.github.com>
2021-11-11 22:11:14 +0700

    up

--- app/resources/js/components/admin/Orders.vue
+++ app/resources/js/components/admin/Orders.vue
@@ -1,242 +1,308 @@
 <template>
-    <div class="o-divide">
-        <div class="o-background-order">
-            <div v-for="value of catagories" :key="value.name" class="com">
-                <div class="o-distance-table">
-                    <div class="o-change-table">
-                            <div class="o-background-des">
-                                <h4 class="o-title">{{ value.name }}</h4>
-                                <p>{{ value.description }}</p>
-                            </div>
-                        <br>
-                        <div class="o-product-table">
-                            <v-data-table
-                                v-model="catagories"
-                                :headers="headers"
-                                :items="value.products"
-                                :items-per-page="5"
-                                class="elevation-1"
-                                @click:row="Khmoc"  single-select
-                            ></v-data-table>
-                        </div>
-                    </div>
-                    <br/>
-                </div>
-            </div>
-        </div>
-        <div class="o-invoice">
-            <div class="o-distance-table">
-                <div class="o-change-table">
-                        <div class="o-background-des">
-                            <h4 class="o-title">{{ invoice }}</h4>
-                        </div>
-                    <br>
-                    <div class="o-product-table-size">
-                        <v-data-table
-                            v-model="detailInvoice"
-                            :headers="invoiceHeader"
-                            :items="detailInvoice"
-                            hide-default-footer
-                        >
-                        <template v-slot:[`item.actions`]="{ item }">
-                            <v-icon
-                            small
-                            class="mr-2"
-                            @click="editItem(item)"
-                            >
-                            mdi-plus
-                            </v-icon>
-                            <v-icon
-                            small
-                            @click="deleteItem(item)"
-                            >
-                            mdi-minus
-                            </v-icon>
+  <v-card height="100%" outlined class="pa-md-4 mx-lg-auto grey lighten-3">
+    <v-data-table
+      :headers="headers"
+      :items="products"
+      :search="search"
+      :expanded.sync="expanded"
+      :single-expand="singleExpand"
+      item-key="name"
+      show-expand
+      rounded-xl
+      class="elevation-1"
+      min-height="100vh"
+      @click:row="clickRow"
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Products</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+                Make Invoice
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.name"
+                        label="Product Name"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.amount"
+                        label="Amount"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.outportPrice"
+                        label="Price"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-textarea v-model="editedItem.description" color="teal">
+                        <template v-slot:label>
+                          <div>Description <small>(optional)</small></div>
                         </template>
-                        </v-data-table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                      </v-textarea>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">
+                  Cancel
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h6"
+                >Are you sure you want to delete this item?</v-card-title
+              >
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete"
+                  >Cancel</v-btn
+                >
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                  >OK</v-btn
+                >
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template #expanded-item="{ headers, item }">
+        <td :colspan="headers.length">
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-img :src="item.poster" :lazy-src="lazy"> </v-img>
+            </v-col>
+            <v-col cols="12" md="8">
+              <h3>{{ item.name }}</h3>
+              <p class="text-justify">
+                {{ item.description }}
+              </p>
+            </v-col>
+          </v-row>
+        </td>
+      </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-icon small class="mr-2" @click="product(item)"> mdi-pencil </v-icon>
+        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <h6 class="text-center">No data found!</h6>
+        <v-btn small fab @click="initialize">
+          <v-icon dark> mdi-restart </v-icon>
+        </v-btn>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
-<style scoped>
-    p {
-        font-size: 14px;
-        padding-left: 14px;
-        color: rgb(218, 218, 211);
-        margin: 5px; 
-    }
-    
-</style>
+
 <script>
-	export default {
-        data(){
-            return {
-                headers: [
-                    {
-                        text: 'Name',
-                        align: 'start',
-                        sortable: false,
-                        value: 'name',
-                        class:'green--text'
-                    },
-                    { text: 'Units', value: 'units', class:'green--text' },
-                    { text: 'Description', value: 'description', class:'green--text'},
-                    { text: 'Price', value: 'price', class:'green--text' },
-                    { text: 'Image', value: 'image', class:'green--text' },
-                ],
-                catagories: [
-                    {
-                        products: [
-                            {
-                                name:"Dell",
-                                units:6,
-                                description:"Dell là dòng máy",
-                                price:111,
-                                image:null
-                            },
-                            {
-                                name:"Dell2",
-                                units:6,
-                                description:"Dell là dòng máy",
-                                price: 2000,
-                                image:null
-                            }
-                        ] ,
-                        name:"Máy tính",
-                        description: "Máy tính là một mặt hàng phổ biến nha",
-                    },
-                    {
-                        products: [
-                            {
-                                name:"GalaxyS1",
-                                units:16,
-                                description:"Galaxy là dòng máy ",
-                                price: 10000,
-                                image:null
-                            },
-                            {
-                                name:"Nokia",
-                                units:10,
-                                description:"Nokia là một dòng máy của mọi nhà của mọi người, vừa đập được đá vừa chọi được người",
-                                price: 100000,
-                                image:null
-                            },
-                            {
-                                name:"cangnha",
-                                units:16,
-                                description:"Galaxy là dòng máy ",
-                                price: 10000,
-                                image:null
-                            },
-                            {
-                                name:"Metmoi",
-                                units:16,
-                                description:"Galaxy là dòng máy ",
-                                price: 10000,
-                                image:null
-                            },
-                        ] ,
-                        name:"Điện thoại",
-                        description: "Điện thoại là một thứ gi đó bây giờ tôi rất cần"
-
-                    }
-
-                ],
-                invoiceHeader:[
-                    {
-                        text: 'Name',
-                        align: 'start',
-                        sortable: false,
-                        value: 'name',
-                        class:'green--text',
-                        width:"1%"
-                    },
-                    { text:'Amount', value:'amount', class:'green--text', width:"1%"},
-                    { text:'Price', value:'price', class:'green--text', width:"1%"},
-                    { text: 'Actions', value: 'actions', sortable: false, class:'green--text', width:"1%"},
-                ],
-                invoice:"Hóa đơn",
-                detailInvoice: [
-                    {
-                        name: "dungkun",
-                        amount: 6,
-                        price: 100000,
-                    }
-                ],
-            }
+export default {
+  data() {
+    return {
+      products: [],
+      search: "",
+      editingItem: null,
+      addingProduct: null,
+      dialog: false,
+      dialogDelete: false,
+      lazy: `https://goo.gl/jbJWmK`,
+      expanded: [],
+      singleExpand: true,
+      headers: [
+        {
+          text: "Name",
+          align: "left",
+          sortable: false,
+          value: "name",
         },
-        components : {
+        { text: "Units", value: "amount" },
+        { text: "Price", value: "outportPrice" },
+        { text: "Actions", value: "actions", sortable: false },
+      ],
+      editedIndex: -1,
+      editedItem: {
+        name: "",
+        amount: 0,
+        importPrice: 10000,
+        outportPrice: 20000,
+        manufacture: "Dungkun",
+        warrantyPeriod: 3,
+        category_id: "Con Cac",
+        description: "Con cac la gi em noi anh xem",
+        tag: ['Dungkun', 'Dungdeptrai'],
+      },
+      defaultItem: {
+        name: "",
+        amount: 0,
+        importPrice: 10000,
+        outportPrice: 20000,
+        manufacture: "Dungkun",
+        warrantyPeriod: 2,
+        category_id: "Con Cac",
+        description: "Con cac la gi em noi anh xem",
+        tag:  ['Dungkun', 'Dungdeptrai'],
+      },
+    };
+  },
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    },
+  },
 
-        },
-        /*beforeMount(){
-            axios.get('/api/products/')
-            .then(response => {
-                this.products = response.data
-            })
-            .catch(error => {
-                console.error(error);
-            })     
-        },*/
-        create (){
-            this.initialize()
-        },
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
 
-        methods: {
-            Khmoc(item, row){
-                let a= this.detailInvoice.map(function(e){
-                    return e.name;
-                }).indexOf(item.name);
-                if(a===-1)
-                    this.detailInvoice.push({name:item.name, amount: 1, price: item.price })
-                else {
-                    this.detailInvoice[a].amount+=1;
-                    this.detailInvoice[a].price+=item.price;
-                }
-            },
-            initialize() {
-            },
-            editItem(item){
-                let temp=""
-                console.log(item.name)
-                let index= this.catagories.map(function(e){
-                    return e.products.map(function(ep) {
-                        return temp==ep.name
-                    }).indexOf(item.name);
-                }).indexOf(item);
-                console.log(index)
-                if(this.catagories[index[1]+1].products[index[0]+1].units > 0) {
-                    this.catagories[index[1]+1].products[index[0]+1].units-=1
-                    item.amount+=1
-                }
-                
-            },
-            deleteItem(item)
-            {
-                if(item.amount<=1){
-                    let index= this.detailInvoice.indexOf(item)
-                    this.detailInvoice.splice(index, 1)
-                    console.log(index)
-                }
-                else item.amount-=1
+  created() {
+    this.initialize();
+  },
 
-            },
-            endEditing(product){
-                this.editingItem = null
-                let index = this.products.indexOf(product)
-                axios.put(`/api/products/${product.id}`,{
-                    name  : product.name,
-                    units : product.units,
-                    price : product.price,
-                    description : product.description,
-                })
-                .then(response =>{
-                    this.products[index] = product
-                })
-                .catch(response => {
-                })
-            },
-        }
-    }
+  beforeMount() {
+    axios
+      .get("/api/products")
+      .then((response) => {
+        this.products = response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+  methods: {
+    clickRow(item, event) {
+      if (event.isExpanded) {
+        const index = this.expanded.findIndex((i) => i === item);
+        this.expanded.splice(index, 1);
+      } else {
+        this.expanded.push(item);
+      }
+    },
+
+    initialize() {
+      axios
+        .get("/api/products/")
+        .then((response) => {
+          this.products = response.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    endEditing(product) {
+      this.editingItem = null;
+      let index = this.products.indexOf(product);
+      axios
+        .put(`/api/products/${product.id}`, {
+          name: this.products.name,
+          amount: this.product.amount,
+          importPrice: this.product.importPrice,
+          outportPrice: this.product.outportPrice,
+          manufacture: this.product.manufacture,
+          warrantyPeriod: this.product.warrantyPeriod,
+          category_id: this.product.category_id,
+          description: this.product.description,
+          tag: this.product.tag,
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    addProduct(product) {
+      axios
+        .post("/api/products", {
+          name: this.products.name,
+          amount: this.product.amount,
+          importPrice: this.product.importPrice,
+          outportPrice: this.product.outportPrice,
+          manufacture: this.product.manufacture,
+          warrantyPeriod: this.product.warrantyPeriod,
+          category_id: this.product.category_id,
+          description: this.product.description,
+          tag: this.product.tag,
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    product(item) {
+      this.editedIndex = this.products.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.products.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      this.products.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.products[this.editedIndex], this.editedItem);
+        this.endEditing(this.editedItem);
+      } else {
+        this.addProduct(this.editedItem);
+        this.products.push(this.editedItem);
+      }
+      this.close();
+    },
+  },
+};
 </script>
