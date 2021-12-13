3d25cfed88dbcc36b9844a5182b58866bbb28205
TruongGiaThach <19522183@gm.uit.edu.vn>
2021-12-13 11:45:06 +0700

    fix_ui_small_button

--- app/resources/js/components/admin/Staffs.vue
+++ app/resources/js/components/admin/Staffs.vue
@@ -189,19 +189,20 @@
               </ValidationObserver>
             </v-dialog>
             <!-- dialog khi xóa 1 row -->
-            <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-dialog v-model="dialogDelete" max-width="600px">
               <v-card>
                 <v-card-title class="text-h5"
-                  >Are you sure you want to delete this item?</v-card-title
+                  >Bạn có chắc muốn xóa thông tin nhân viên này?</v-card-title
                 >
                 <v-card-actions>
                   <v-spacer></v-spacer>
+                  <v-btn color="error" text @click="deleteItemConfirm"
+                    >Xóa</v-btn
+                  >
                   <v-btn color="blue darken-1" text @click="closeDelete"
                     >Hủy</v-btn
                   >
-                  <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                    >OK</v-btn
-                  >
+
                   <v-spacer></v-spacer>
                 </v-card-actions>
               </v-card>
@@ -210,7 +211,7 @@
         </template>
         <!-- expand 1 row -->
         <template v-slot:expanded-item="{ headers, item }">
-          <td :colspan="headers.length">
+          <td :colspan="headers.length" v-if="item.email != `admin@devtest.com`">
             <v-container fluid>
               <v-row no-gutters>
                 <table class="table_style">
@@ -246,10 +247,23 @@
         </template>
         <!-- delete edit button -->
         <template v-slot:[`item.actions`]="{ item }">
-          <v-icon small class="mr-2" @click="editItem(item)">
+          <v-icon
+            small
+            class="mr-2"
+            @click="editItem(item)"
+            color="info"
+            v-if="item.email != `admin@devtest.com`"
+          >
             mdi-pencil
           </v-icon>
-          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+          <v-icon
+            small
+            @click="deleteItem(item)"
+            color="error"
+            v-if="item.email != `admin@devtest.com`"
+          >
+            mdi-delete</v-icon
+          >
         </template>
         <!-- reload -->
         <template v-slot:no-data>
